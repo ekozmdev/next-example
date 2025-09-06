104beeff1731d22f15be1e367c3a68b877c037f3
ekozmdev <130561749+ekozmdev@users.noreply.github.com>
2025-09-06 23:08:41 +0900

    section2-10: 片付け

--- src/app/(admin)/dashboard/page.tsx
+++ src/app/(admin)/dashboard/page.tsx
@@ -1,8 +1,4 @@
 const AdminDashboardPage = () => {
-  // 強制的にエラーを発生させると、そのtsxに一番近いerror.tsxが表示される
-  // ２番目以降は表示されないようだ
-  throw new Error('意図的にエラーを発生させています');
-
   return (
     <div>AdminDashboardPage</div>
   );
