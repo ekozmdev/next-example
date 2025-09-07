957f514b4c354f4bd074011e8a2af7de83e50460
ekozmdev <130561749+ekozmdev@users.noreply.github.com>
2025-09-07 14:18:01 +0900

    section2-14

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,18 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    logging: {
+        // fetchがキャッシュされているかどうかをログに出す設定
+        // キャッシュあり
+        //  GET /task 200 in 19ms
+        //  │ GET http://localhost:3000/api/tasks 200 in 2ms (cache hit)
+        // キャッシュなし
+        //  GET /task 200 in 17ms
+        //  │ GET http://localhost:3000/api/tasks 200 in 6ms (cache skip)
+
+        fetches: {
+            fullUrl: true
+        }
+    }
+};
 
 export default nextConfig;
--- src/app/(main)/task/page.tsx
+++ src/app/(main)/task/page.tsx
@@ -1,7 +1,25 @@
-// ファイル名と一致してなくてもOK
-const TaskPage = () => {
+import { Task } from "@/app/api/tasks/route";
+
+const getTasks = async () => {
+  const response = await fetch("http://localhost:3000/api/tasks", {
+    method: "GET",
+    cache: "no-store" // キャッシュを無効化して、常に最新のデータを取得する
+  });
+  return await response.json();
+}
+
+// asyncのコンポーネントはサーバーコンポーネントでしか使えん
+const TaskPage = async () => {
+  const tasks = (await getTasks()).tasks as Task[];
   return (
-    <div>TaskPage</div>
+    <div>
+      <div>TaskPage</div>
+      <div>
+        {tasks.map((task) => (
+          <div key={task.id}>{task.name}</div>
+        ))}
+      </div>
+    </div>
   );
 }
 
