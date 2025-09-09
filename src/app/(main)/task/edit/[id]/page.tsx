9858e034fe09338663f5dc46f54bb3f22f51690f
ekozmdev <130561749+ekozmdev@users.noreply.github.com>
2025-09-09 22:33:59 +0900

    section: 6-53

--- src/app/(main)/task/edit/[id]/page.tsx
+++ src/app/(main)/task/edit/[id]/page.tsx
@@ -1,6 +1,7 @@
 // 動的ルーティングは[]で囲んだフォルダ名にする
 // []の中の名前でpropsとして受け取れる
 const TaskEditIdPage = async (props: { params: Promise<{id: string}> }) => {
+  // Next15からparamsが非同期化するので、awaitする必要がある。
   const params = await props.params;
   return (
     <div>task id: { params.id }</div>
